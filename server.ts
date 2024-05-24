37544b40e143b319fc13ab7fc6914d0ef66ae4d0
Giuseppe Buoncore <135731752+GiuseppeBuoncore@users.noreply.github.com>
2024-05-24 06:38:29 -0700

    Add files via upload

---
+++ server.ts
@@ -0,0 +1,30 @@
+import express from "express";
+import morgan from "morgan";
+import "express-async-errors";
+import {
+  getAll,
+  getOneById,
+  create,
+  updateById,
+  deleteById,
+} from "./controllers/planets.js";
+
+const app = express();
+const port = 3000;
+
+app.use(morgan("dev"));
+app.use(express.json());
+
+app.get("/api/planets", getAll);
+
+app.get("/api/planets/:id", getOneById);
+
+app.post("/api/planets", create);
+
+app.put("/api/planets/:id", updateById);
+
+app.delete("/api/planets/:id", deleteById);
+
+app.listen(port, () => {
+  console.log(`server running on http://localhost:${port}`);
+});
